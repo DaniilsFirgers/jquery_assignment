bde24d4a834c3ec83d927467359b698c6c2fcd54
Daniils Firgers <dfirger@gmail.com>
2023-05-05 16:36:13 +0300

    add logic to session cookies

--- about/about.js
+++ about/about.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+  let currentCookie = Cookies.get("sessionToken");
+  if (currentCookie === undefined) {
+    $("#currentSessionStatus").text("No active sessions!");
+    $(".session-status").text(false);
+  } else {
+    $("#session-token").text(`Current session token: ${currentCookie}`);
+    $(".session-status").text(true);
+  }
   $("#home-page").click();
   $("#components-page").click();
 });
--- home/home.js
+++ home/home.js
@@ -1,8 +1,29 @@
 $(document).ready(function () {
+  let currentCookie = Cookies.get("sessionToken");
+  console.log(currentCookie);
+  if (currentCookie === undefined) {
+    $("#currentSessionStatus").text("No active sessions!");
+    $(".session-status").text(false);
+  } else {
+    $("#currentSessionStatus").text("One active cookie session!");
+    $("#session-token").text(`Current session token: ${currentCookie}`);
+    $(".session-status").text(true);
+  }
+
   $("#about-page").click();
   $("#components-page").click();
   $("#add-cookie-button").click(function () {
+    if (currentCookie !== undefined) return;
     let token = uuidv4();
+    //chekc if already set
     Cookies.set("sessionToken", token, { expires: 1 / 144 });
+    location.reload();
+  });
+
+  $("#delete-cookie-button").click(function () {
+    if (currentCookie === undefined) return;
+    //chekc if already set
+    Cookies.remove("sessionToken");
+    location.reload();
   });
 });
